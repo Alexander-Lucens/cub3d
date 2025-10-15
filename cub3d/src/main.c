b014263e57232f99100858f4ed3329375fc64750
Alexander Kuzmin <alexanderkuzmin@Alexanders-iMac.fritz.box>
2025-10-15 17:46:23 +0200

    Modified move action for key press now its work with key hold and release. Need to be modified for use with float in cib3d

--- cub3d/src/main.c
+++ cub3d/src/main.c
@@ -32,15 +32,17 @@ int	main(int ac, char **av)
 	init_data(game, av[1]);
 
 	ft_bzero(&game->controls, sizeof(t_controls));
+
+	// Get key press and move in cub3d make it work with float numbers
 	game->move_cooldown = 0;
-	// Регистрируем хуки
 	mlx_hook(game->graphics.win, 2, 1L << 0, key_press, game);
 	mlx_hook(game->graphics.win, 3, 1L << 1, key_release, game);
 	mlx_hook(game->graphics.win, 17, 0, close_game, game);
 	mlx_loop_hook(game->graphics.mlx, update, game);
 
 	mlx_loop(game->graphics.mlx);
-
+	// --------------------------------------------
+	
 	close_game(game);
 	return (0);
 }
