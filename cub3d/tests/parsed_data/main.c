591af13a230ee9705a3d3d6a72ea2ea76ff82459
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2025-10-24 21:56:09 +0200

    test: added parsing tests
    What's new?
    - added three parsing tests to unit test current parsing functions
    - adjusted the maps files to find the texture folder
    -adjusted Makefile accordingly to new parsing functions

--- cub3d/tests/parsed_data/main.c
+++ cub3d/tests/parsed_data/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akuzmin <akuzmin@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: lkramer <lkramer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 02:19:22 by akuzmin           #+#    #+#             */
-/*   Updated: 2025/10/24 07:15:12 by akuzmin          ###   ########.fr       */
+/*   Updated: 2025/10/24 21:44:09 by lkramer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,19 @@ t_parsed_data	*fake_parser(char *path)
 int main(void)
 {
 	int ret;
+
+	ft_printf("=== PARSER TESTING SUITE ===\n\n");
 	
 	ft_printf("----- Starts validation of parser -----\n");
-	
-	// ret = run_as_validator(/* PLACE FOR PARSER FUNCTION */);
 	ret = run_as_validator(fake_parser);
+
+	/* ft_printf("----- Testing real parser -----\n");
+    ret = run_as_validator(test_parser_wrapper);   */
+
+	ft_printf("----- Testing individual parser functions -----\n");
+    test_parse_texture_path();
+    test_parse_color();
+	test_parse_cub_file();
 	
 	ft_printf("\n------- END OF VALIDATION -------\n");
 	return (ret);	
--- cub3d/tests/parsed_data/tests.c
+++ cub3d/tests/parsed_data/tests.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tests.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akuzmin <akuzmin@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: lkramer <lkramer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 02:19:22 by akuzmin           #+#    #+#             */
-/*   Updated: 2025/10/24 07:11:00 by akuzmin          ###   ########.fr       */
+/*   Updated: 2025/10/24 21:43:20 by lkramer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,4 +52,97 @@ int	parser_test_map2(t_parsed_data *origin)
 	ret = parser_test_map(data);
 	ft_printf(" --- END OF TEST ---\n");
 	return (ret);
+}
+
+int test_parse_texture_path()
+{
+    t_game game = {0};
+    int result;
+    
+	ft_printf(" --- Parser test 3 ---\n");
+    ft_printf("Testing parse_texture_path...\n");
+    
+    result = parse_texture_path(&game, "../textures/wn0.xpm", NORTH_TEXTURE);
+    if (result && game.data.north_texture_path)
+		return (PRINT_SUCCESS("North texture parsed successfully."), 0);
+    else
+        return (PRINT_FAIL("North texture parsing failed."), 1);
+    
+    result = parse_texture_path(&game, "./nonexistent.xpm", SOUTH_TEXTURE);
+	ft_printf(" --- END OF TEST ---\n");
+    if (!result)
+        return (PRINT_SUCCESS("Correctly rejected file."), 0);
+    else
+        return (PRINT_FAIL("Should have rejected file."), 1);
+}
+
+int test_parse_color()
+{
+    t_game game = {0};
+    int result;
+    
+	ft_printf(" --- Parser test 4 ---\n");
+    ft_printf("Testing parse_color_path...\n");
+    
+    result = parse_color_path(&game, "255,128,0", FLOOR_COLOR);
+    if (result && game.data.floor)
+        return (PRINT_SUCCESS("Correctly passed colors."), 0);
+    else
+        return (PRINT_FAIL("Failed to parse color path."), 1);
+    
+    result = parse_color_path(&game, "300,128,0", CEILING_COLOR);
+	ft_printf(" --- END OF TEST ---\n");
+    if (!result)
+        return (PRINT_SUCCESS("Correctly rejected path."), 0);
+    else
+        return (PRINT_FAIL("Should have rejected path, out of range."), 1);
+}
+
+int test_parse_cub_file()
+{
+    t_game *game;
+    t_game *result;
+    int test_failures = 0;
+    
+    ft_printf(" --- Parser test 5 ---\n");
+    ft_printf("Testing parse_cub_file...\n");
+    
+    game = ft_calloc(1, sizeof(t_game));
+    if (!game)
+    {
+        PRINT_FAIL("Failed to allocate game");
+        return (1);
+    }
+    
+    result = parse_cub_file(game, "../maps/map0.cub");
+    if (result)
+        PRINT_SUCCESS("Successfully parsed map0.cub");
+    else
+    {
+        PRINT_FAIL("Failed to parse map0.cub");
+		ft_printf("Debug: parse_cub_file returned NULL\n");
+        test_failures++;
+    }
+    if (result)
+    {
+        if (result->data.north_texture_path) free(result->data.north_texture_path);
+        if (result->data.south_texture_path) free(result->data.south_texture_path);
+        if (result->data.west_texture_path) free(result->data.west_texture_path);
+        if (result->data.east_texture_path) free(result->data.east_texture_path);
+        if (result->data.floor) free(result->data.floor);
+        if (result->data.ceiling) free(result->data.ceiling);
+        free(result);
+    }
+
+	game = ft_calloc(1, sizeof(t_game));
+    result = parse_cub_file(game, "../maps/nonexistent.cub");
+    if (!result)
+        PRINT_SUCCESS("Correctly rejected non-existent file");
+    else
+	{
+        PRINT_FAIL("Should have rejected non-existent file");
+        test_failures++;
+    }
+    ft_printf(" --- END OF TEST ---\n");
+    return (test_failures);
 }
