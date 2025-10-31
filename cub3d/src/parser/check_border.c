0cd16910cb1e7c6a5759d937b9a8b1b125ae0ff7
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2025-10-31 17:52:40 +0100

    feature: add first version of complete parser logic
    - handles texture validation and path (colors, texture elements)
    -handles regular and irregular shaped map validation and parsing.
    - added unit tests in /tests

--- cub3d/src/parser/check_border.c
+++ cub3d/src/parser/check_border.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   check_border.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akuzmin <akuzmin@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: lkramer <lkramer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 07:54:10 by akuzmin           #+#    #+#             */
-/*   Updated: 2025/10/24 05:05:44 by akuzmin          ###   ########.fr       */
+/*   Updated: 2025/10/31 17:07:02 by lkramer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+
+void	check_accessibility(t_game *game)
+{
+	t_dfs	data;
+	int		i;
+
+	i = 0;
+	data.visited = malloc(sizeof(int *) * game->map.map_height);
+	while (i < game->map.map_height)
+	{
+		data.visited[i] = calloc(game->map.map_width, sizeof(int));
+		i++;
+	}
+	if (dfs(*game, (int)game->player.position.x,
+			(int)game->player.position.y, &data)) 
+	{
+		free_dfs_visited(&data, game->map.map_height);
+		return ;
+	}
+	free_dfs_visited(&data, game->map.map_height);
+}
 
 t_pos	init_tpos(int x, int y)
 {
@@ -21,26 +42,50 @@ t_pos	init_tpos(int x, int y)
 	return (out);
 }
 
+static int	is_on_map_edge(t_game *game, int row, int col)
+{
+	if (row == 0 || row == game->map.map_height - 1)
+		return (1);
+	if (col == 0 || col == game->map.map_width - 1)
+		return (1);
+	if (row > 0 && (game->map.matrix[row - 1][col] == ' '
+		|| !game->map.matrix[row - 1][col] == 0))
+		return (1);
+	if (row < game->map.map_height - 1 && (game->map.matrix[row + 1][col] == ' '
+		|| !game->map.matrix[row + 1][col] == 0))
+		return (1);
+	if (col > 0 && (game->map.matrix[row][col - 1] == ' '
+		|| !game->map.matrix[row][col - 1] == 0))
+		return (1);
+	if (col < game->map.map_width - 1 && (game->map.matrix[row][col + 1] == ' '
+		|| !game->map.matrix[row][col + 1] == 0))
+		return (1);
+	return (0);
+}
+
 void	check_border(t_game *game)
 {
-	t_pos	d;
-	t_pos	p;
-	t_pos	n;
+	int	i;
+	int	j;
 
-	p = init_tpos(0, 0);
-	d = init_tpos(1, 0);
-	n = init_tpos(p.x + d.x, p.y + d.y);
-	while (n.x != 0 || n.y != 0)
+	i = 0;
+	while (i < game->map.map_height)
 	{
-		if (n.x < game->map.cols && n.y < game->map.rows \
-			&& n.x >= 0 && n.y >= 0)
+		j = 0;
+		while (j < game->map.map_width)
 		{
-			p = init_tpos(n.x, n.y);
-			if (game->map.matrix[p.y][p.x] != '1')
-				ft_error(game, 104);
+			if ((game->map.matrix[i][j] == 0
+				|| game->map.matrix[i][j] == 'N'
+				|| game->map.matrix[i][j] == 'S'
+				|| game->map.matrix[i][j] == 'E'
+				|| game->map.matrix[i][j] == 'W')
+				&& is_on_map_edge(game, i, j))
+			{
+				return ;
+				// ft_error(game, 104);
+			}
+			j++;
 		}
-		else
-			d = init_tpos(-(d.y), d.x);
-		n = init_tpos(p.x + d.x, p.y + d.y);
+		i++;
 	}
 }
